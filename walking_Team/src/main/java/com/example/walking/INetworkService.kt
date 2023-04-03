baa0b7f5ccb395e2366541e2052169fbbda4c418
MinuSong2022 <smw9885464@gmail.com>
2023-04-03 17:48:53 +0900

    0403 1747

---
+++ walking_Team/src/main/java/com/example/walking/INetworkService.kt
@@ -0,0 +1,46 @@
+package com.example.walking
+
+
+import com.example.walking.model.*
+
+import retrofit2.Call
+import retrofit2.http.*
+
+interface INetworkService {
+    @POST("walking/user/join")
+    fun doInsertUser(@Body user: User?): Call<User>
+
+    @POST("login")
+    fun login(@Body loginDto: LoginDto): Call<LoginDto>
+
+    @GET("walking/user/oneUser")
+    fun doGetOneUser(@Query("email") email: String?): Call<User>
+
+
+
+    //meeting
+    @GET("walking/meeting/list")
+    fun doGetMeetingList(): Call<MeetingListModel>
+
+    @GET("walking/meeting/oneMeeting")
+    fun doGetOneMeeting(@Query("title") title:String?): Call<Meeting>
+
+    @POST("walking/meeting/insert")
+    fun doInsertMeeting(@Body meeting: Meeting?): Call<Meeting>
+
+    @POST("walking/meeting/insertuserinmeeting")
+    fun doInsertUserinmeeting(@Body userinmeeting: Userinmeeting?): Call<Userinmeeting>
+
+    @GET("walking/meeting/oneUserinmeeting")
+    fun doGetOneUserinmeeting(@Query("userinmeeting_val") userinmeeting_val:String?): Call<Userinmeeting>
+
+    @GET("walking/meeting/chatMemberList")
+    fun doGetChatMemberList(@Query("meeting_id") meeting_id:Int?): Call<List<User>>
+
+
+    @POST("walking/board/insert")
+    fun doInsertBoard(@Body board: Board?): Call<Board>
+
+    @GET("walking/board/list")
+    fun doGetBoardList(): Call<BoardListModel>
+}
